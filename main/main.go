1dd1ece6caba89fe6d71b808105be974db7b95d7
Darinela <darynella2001@gmail.com>
2021-09-26 23:42:53 +0300

    Updating main

--- main/main.go
+++ main/main.go
@@ -4,7 +4,19 @@ import (
 	"net/http"
 	"io"
 )
-
+type Food struct {
+	Id               int              `json:"id"`
+	Name             string           `json:"name"`
+	PreparationTime  int              `json:"preparation-time"`
+	Complexity       int              `json:"complexity"`
+	CookingApparatus string           `json:"cooking-apparatus"`
+}
+type Order struct {
+	Id       int    `json:"id"`
+	Items    []int  `json:"items"`
+	Priority int    `json:"priority"`
+	MaxWait  int    `json:"maxWait"`
+}
 func main() {
 	http.HandleFunc("/", servePage)
 	http.ListenAndServe(":8080", nil)
